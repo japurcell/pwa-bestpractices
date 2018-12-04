3f476a84248cd3d29c3c44781fc2528016f47041
japurcell <japurcell@users.noreply.github.com>
2018-12-03 20:08:46 -0800

    Adding more foundation

---
+++ README.md
@@ -0,0 +1,6 @@
+## References
+[App Shell Credit](https://github.com/googlecodelabs/your-first-pwapp/)
+[App Shell Example](https://github.com/GoogleChromeLabs/application-shell)
+[Google Dev Training](https://developers.google.com/web/ilt/pwa/)
+[Web Fundamentals](https://developers.google.com/web/fundamentals/codelabs/your-first-pwapp/#architect_your_app_shell)
+[Request](https://developer.mozilla.org/en-US/docs/Web/API/Request)
--- src/sw.js
+++ src/sw.js
@@ -1,17 +1,76 @@
-const cacheName = 'shell-content';
+const cacheName = 'my-site-cache-v1';
 const filesToCache = [
+  '/',
   '/styles/inline.css',
   'app.js'
 ];
 
+const logSw = msg =>
+  console.log(`[ServiceWorker] ${msg}`);
+
+  // install should cache shell
 self.addEventListener('install', e =>
 {
-  console.log('[SeviceWorker] Install');
+  logSw('Install');
   e.waitUntil(
     caches.open(cacheName).then(cache =>
       {
-        console.log('[ServiceWorker] Caching app shell');
+        logSw('Caching app shell');
         return cache.addAll(filesToCache);
       })
   );
 });
+
+// activate clears old caches
+self.addEventListener('activate', e =>
+{
+  logSw('Activate');
+
+  e.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key =>
+        {
+          if (key !== cacheName)
+          {
+            logSw(`Removing old cache ${key}`);
+            return caches.delete(key);
+          }
+        })))
+  );
+
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', e =>
+  e.respondWith(
+    caches.match(e.request).then(response =>
+      {
+        const url = e.request.url;
+
+        if (response)
+        {
+          logSw(`Responding with cached response for ${url}`);
+          return response;
+        }
+
+        logSw(`Responding with fetched response for ${url}`);
+        const fetchRequest = e.request.clone();
+
+        return fetch(fetchRequest).then(response =>
+          {
+            if (!response || response.status !== 200 || response.type !== 'basic')
+            {
+              logSw(`Not caching the response for this request: ${url}`);
+              return response;
+            }
+
+            logSw(`Caching response for this request: ${url}`);
+            const responseToCache = response.clone();
+
+            caches.open(cacheName).then(cache =>
+              cache.put(e.request, responseToCache));
+
+            return response;
+          });
+      })
+  ));
