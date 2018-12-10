b7e3640ee3734effe2f625ae5370f8c7cbcd2a8f
japurcell <japurcell@users.noreply.github.com>
2018-12-09 17:03:10 -0800

    Experimenting with flex

--- src/app.js
+++ src/app.js
@@ -2,9 +2,48 @@
 {
   'use strict';
 
-  var app = {
+  const app = {
+
+    loadItems: (initialItems) =>
+    {
+      const main = document.querySelector('.main');
+
+      initialItems.forEach(item =>
+        {
+          const el = document.createElement('div');
+          el.classList.add('card');
+
+          const elTitle = document.createElement('h3');
+          elTitle.classList.add('card-title');
+          elTitle.innerText = item.id;
+
+          const elContent = document.createElement('div');
+          elContent.classList.add('card-body');
+          elContent.innerText = item.name;
+
+          el.appendChild(elTitle);
+          el.appendChild(elContent);
+
+          main.appendChild(el);
+        });
+    },
+
+    seed: () =>
+    {
+      const items = [];
+
+      for (let i = 0; i < 9; i++)
+      {
+        items.push({ id: i, name: `Item ${i}`});
+      }
+
+      return items;
+    }
 
   };
+
+  const initialItems = app.seed();
+  app.loadItems(initialItems);
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
